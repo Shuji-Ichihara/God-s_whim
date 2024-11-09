0df4a70f8472c96bbef18d176b540fb70aab6002
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-09 18:28:23 +0900

    Create Common
    頻出するデータを管理するクラスを作成した。

---
+++ Assets/Member/Scripts/Common.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class Common
+{
+    #region Variables
+    // 使用しているスプライト画像が16*16である為。
+    public static readonly float StandardValue = 1.6f;
+    #endregion
+}
