b032e6dbf50bc86ed28d71f4af432096812009d1
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-10 01:52:15 +0900

    Update Field.cs
    オブジェクトを破棄する座標を調整した。

--- Assets/Member/Scripts/Field.cs
+++ Assets/Member/Scripts/Field.cs
@@ -13,11 +13,10 @@ public class Field : MonoBehaviour
     private async UniTask MoveField(float moveTime, CancellationTokenSource cts)
     {
         // アタッチされているフィールドオブジェクトを破棄する座標
-        float deleteCoodinateX = -20f;
+        float deleteCoodinateX = -23f;
         while (transform.position.x > deleteCoodinateX)
         {
             // -方向に移動させる為、Vector3.leftを使用。
-            //transform.position += Vector3.left * (generateCoodinateX * 2f / moveTime) * Time.deltaTime;
             transform.position += Vector3.left * (Common.StandardValue * Common.FieldWidth / moveTime) * Time.deltaTime;
             await UniTask.Yield(cancellationToken: cts.Token);
         }
