c286663199f84f9377f410b59dfcc3015c5e90c8
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-10 01:09:06 +0900

    Update Common.cs
    使用してない参照を削除し、Fieldプレハブのスプライト画像の枚数を書くのする変数を記述した。

--- Assets/Member/Scripts/Common.cs
+++ Assets/Member/Scripts/Common.cs
@@ -1,12 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public static class Common
+﻿public static class Common
 {
     #region Variables
     // 使用しているスプライト画像が16*16である為。
     public static readonly float StandardValue = 1.6f;
+    // フィールドを構成しているスプライトの横の枚数
+    public static readonly int FieldWidth = 14;
     #endregion
 }
