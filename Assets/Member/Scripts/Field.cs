9e8b16af1521166b498b2f5373ac17adfc93b096
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-10 01:12:33 +0900

    Create Field
    フィールドのプレハブをスクロールさせるクラスを作成した。

---
+++ Assets/Member/Scripts/Field.cs
@@ -0,0 +1,26 @@
+﻿using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    public void MoveFieldWrap(float moveTime)
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        MoveField(moveTime, cts).Forget();
+    }
+
+    private async UniTask MoveField(float moveTime, CancellationTokenSource cts)
+    {
+        // アタッチされているフィールドオブジェクトを破棄する座標
+        float deleteCoodinateX = -20f;
+        while (transform.position.x > deleteCoodinateX)
+        {
+            // -方向に移動させる為、Vector3.leftを使用。
+            //transform.position += Vector3.left * (generateCoodinateX * 2f / moveTime) * Time.deltaTime;
+            transform.position += Vector3.left * (Common.StandardValue * Common.FieldWidth / moveTime) * Time.deltaTime;
+            await UniTask.Yield(cancellationToken: cts.Token);
+        }
+        Destroy(gameObject);
+    }
+}
