842f21225b1d048fc2264a126e08681453627091
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-11 17:45:59 +0900

    Update Field.cs
    MoveFieldをScrollFieldへ名称を変更した。

--- Assets/Member/Scripts/Field.cs
+++ Assets/Member/Scripts/Field.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 
 public class Field : MonoBehaviour
 {
-    public void MoveFieldWrap(float moveTime)
+    /// <summary>
+    /// ScrollFieldのラッパー関数
+    /// </summary>
+    /// <param name="moveSecond">画面をスクロールする秒数</param>
+    public void ScrollFieldWrap(float moveSecond)
     {
         CancellationTokenSource cts = new CancellationTokenSource();
-        MoveField(moveTime, cts).Forget();
+        ScrollField(moveSecond, cts).Forget();
     }
 
-    private async UniTask MoveField(float moveTime, CancellationTokenSource cts)
+    /// <summary>
+    /// アタッチされているフィールドをスクロールする
+    /// </summary>
+    /// <param name="moveSecond">画面をスクロールする秒数</param>
+    /// <param name="cts"></param>
+    /// <returns></returns>
+    private async UniTask ScrollField(float moveSecond, CancellationTokenSource cts)
     {
         // アタッチされているフィールドオブジェクトを破棄する座標
         float deleteCoodinateX = -23f;
         while (transform.position.x > deleteCoodinateX)
         {
             // -方向に移動させる為、Vector3.leftを使用。
-            transform.position += Vector3.left * (Common.StandardValue * Common.FieldWidth / moveTime) * Time.deltaTime;
+            transform.position += Vector3.left * (Common.StandardValue * Common.FieldWidth / moveSecond) * Time.deltaTime;
             await UniTask.Yield(cancellationToken: cts.Token);
         }
         Destroy(gameObject);
