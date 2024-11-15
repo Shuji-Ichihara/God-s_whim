220011744dee4b9c6eb4b9617dd7c6e129e2a5f2
masatometoki <alicedrop7716@gmail.com>
2024-11-15 18:22:23 +0900

    Update WatchUIController
    時計の動きを時間停止と絡めさせられるように作成中。

--- Assets/Member/Metoki/Scripts/WatchUIController.cs
+++ Assets/Member/Metoki/Scripts/WatchUIController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class WatchUIController : MonoBehaviour
@@ -10,22 +11,61 @@ public class WatchUIController : MonoBehaviour
     //一周にかかる時間
     [SerializeField]
     private float _routationDuration;
+    //記録する時間
+    [SerializeField]
+    private float _recordDuration;
+
+    private List<ClockHnadState> stateHistry = new List<ClockHnadState>();
+
+    //巻き戻し中かどうか
+    private bool _isRewdining = false;
+
+    //回転の経過時間
+    [SerializeField]
+    private float _elppsedRotateionTime;
+
+
+    //記録用の構造体
+    private struct ClockHnadState
+    {
+        public Quaternion _rotation;
+
+        public ClockHnadState(Quaternion rot)
+        {
+            _rotation = rot;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         //最初針の位置を12時に固定
         _hourHand.localRotation = Quaternion.Euler(0, 0, 0);
-        //一周させる処理開始
-        StartCoroutine(RotateHand());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            _isRewdining = true;
+        }
+        else if(Input.GetKeyUp(KeyCode.R))
+        {
+            _isRewdining = false;
+        }
+
+        if(_isRewdining)
+        {
+            Rwind();
+        }
+        else
+        {
+            Record();
+            RotateHand();
+        }
     }
 
-    private IEnumerator RotateHand()
+    void RotateHand()
     {
         float elpsedTime = 0;
         while (elpsedTime < _routationDuration)
@@ -35,9 +75,19 @@ public class WatchUIController : MonoBehaviour
             _hourHand.localRotation = Quaternion.Euler(0, 0, -angle);
 
             elpsedTime += Time.deltaTime;
-            yield return null;
-        }
+            //yield return null;
+       }
         //5秒後に時計の針を一周させて終了
         _hourHand.localRotation = Quaternion.Euler(0, 0, -360f);
+    }
+
+    void Rwind()
+    {
+
+    }
+
+    void Record()
+    {
+
     }
 }
