e64e598a3475aaace2f6cc7288c21af504d9a774
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-16 17:23:19 +0900

    Update Common.cs
    ギミックの名称の変数を定義した。

--- Assets/Member/Scripts/Common.cs
+++ Assets/Member/Scripts/Common.cs
@@ -1,9 +1,15 @@
 ﻿public static class Common
 {
     #region Variables
+    public static readonly int Half = 2;
     // 使用しているスプライト画像が16*16である為。
     public static readonly float StandardValue = 1.6f;
     // フィールドを構成しているスプライトの横の枚数
     public static readonly int FieldWidth = 14;
+    // ギミックの名称
+    public static readonly string HoleGimmickName   = "_Hole";
+    public static readonly string NeedleGimmickName = "_Needle";
+    public static readonly string SlopeGimmickName  = "_Slope";
+
     #endregion
 }
