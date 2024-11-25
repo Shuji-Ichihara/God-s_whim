6ea79f3e6307755808aaf3059801fe861b3fa583
masatometoki <21c1020049mm@edu.tech.ac.jp>
2024-11-25 14:18:20 +0900

    Update ScoreDisplay
    スコアを表示するスクリプトと、100mごとに○○m到達!と明記するスクリプト作成

---
+++ Assets/Member/Metoki/Scripts/ScoreDisplay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private TextMeshProUGUI milestoneText;   // 節目メッセージ表示用
+
+    private void Start()
+    {
+        // ScoreManagerのイベントを購読
+        if (ExsanpleScore.Instance != null)
+        {
+            ExsanpleScore.Instance.OnMilestoneReached += ShowMilestoneMessage;
+        }
+
+        // 節目メッセージを非表示にしておく
+        milestoneText.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (ExsanpleScore.Instance != null)
+        {
+            scoreText.text = "Score: " + ExsanpleScore.Instance.GetScore().ToString();
+        }
+    }
+
+    private void ShowMilestoneMessage(int milestoneScore)
+    {
+        // 節目メッセージの更新と表示
+        milestoneText.text = $"{milestoneScore}m到達!";
+        milestoneText.gameObject.SetActive(true);
+
+        // 2秒後に非表示
+        Invoke(nameof(HideMilestoneMessage), 2f);
+    }
+
+    private void HideMilestoneMessage()
+    {
+        milestoneText.gameObject.SetActive(false);
+    }
+}
