fd9217b19bd9517db955706c455a3114ac1bb24b
Shuji-Ichihara <Furin.kazan.7050@gmail.com>
2024-11-30 03:57:21 +0900

    Create Bomb
    爆弾のスクリプトを作成した。

---
+++ Assets/Member/Ichihara/Scripts/Bomb.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // 破壊可能オブジェクトの場合は自身と対象のオブジェクトを破棄する
+        if (other.gameObject.name.Contains("Breakable"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+        // プレイヤーに当たった場合はゲームオーバー
+        var unityChan = other.gameObject.GetComponent<UnityChanAnim>();
+        if (unityChan != null)
+        {
+            Destroy(gameObject);
+            StartCoroutine(unityChan.Damage());
+        }
+    }
+}
